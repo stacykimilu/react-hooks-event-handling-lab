3e15c809f5781a13787ed9ee660587be90f2ebdb
Stacy <stacykimilu@gmail.com>
2024-05-13 18:04:50 +0300

    completed

---
+++ src/components/EyesOnMe.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Keypad extends React.Component {
+  handleChange = () => {
+    console.log('Entering password...');
+  };
+
+  render() {
+    return <input type="password" onChange={this.handleChange} />;
+  }
+}
+
+export default Keypad;
---
+++ src/components/Keypad.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+  handleFocus = () => {
+    console.log('Good!');
+  };
+
+  handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+  };
+
+  render() {
+    return (
+      <button onFocus={this.handleFocus} onBlur={this.handleBlur}>
+        Eyes on me
+      </button>
+    );
+  }
+}
+
+export default EyesOnMe;
